05d06c53e639c84ab90a6e8417de9772a5a2e358
maxivega0 <maximilianovega600@gmail.com>
2023-07-04 16:07:41 -0300

    agregamos un puerto a la config del index

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 import express from 'express'
 
-console.log("cuarta prueba de backend");
+// Usar un puerto
+const app = express();
+app.set('port', process.env.PORT || 4000)
+app.listen( app.get("port"), ()=>{
+    console.log("Estoy en el puerto "+app.get("port"));
+});
+
+// middlewares: funcoines que se ejecutan antes de las rutas
+
+// rutas
+
