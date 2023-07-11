d76a83d5162c8e5f1dacaf208781a94c353325fa
maxivega0 <maximilianovega600@gmail.com>
2023-07-11 15:30:43 -0300

    agregamos validacion al campo categoria y demas

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -22,8 +22,34 @@ router
       check("nombreProducto")
         .notEmpty()
         .withMessage("El nombre del producto es obligatorio")
-        .isLength({min:2, max:100})
-        .withMessage('El nombre del producto debe tener entre 2 y 100 caracteres')
+        .isLength({ min: 2, max: 32 })
+        .withMessage(
+          "El nombre del producto debe tener entre 2 y 100 caracteres"
+        ),
+      check("precio")
+        .notEmpty()
+        .withMessage("El precio es un dato obligatorio")
+        .isNumeric()
+        .withMessage("El precio debe ser un número ")
+        .custom((value) => {
+          if (value >= 1 && value <= 10000) {
+            return true;
+          } else {
+            throw new Error("El precio debe estar entre 1 y 10000");
+          }
+        }),
+      check("imagen")
+        .isEmpty()
+        .withMessage("La url de imagen es obligatoria")
+        .matches(/^https?:\/\/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(\/[^\s]*)?$/)
+        .withMessage(
+          'La URL debe comenzar con "http://" o "https://", seguido de un dominio válido y una ruta opcional.'
+        ),
+        check('categoria')
+        .isEmpty()
+        .withMessage('La categoria es obligatoria')
+        .isIn('bebida caliente', 'bebida fria', 'dulce', 'salado')
+        .withMessage('Debe ingresar una categoria valida.')
     ],
     crearProducto
   ); // solo puedo poner una de cada tipo(get, etc)
