2e0eaae6f2a3971e9028ac113ed2e4b3b0b89d87
maxivega0 <maximilianovega600@gmail.com>
2023-07-13 17:33:24 -0300

    format al doc

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@ import "dotenv/config"; // hace que funcione el archivo process
 
 export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost:27017/cafecito_db";
-console.log(MONGODB_URI);
+// console.log(MONGODB_URI);
